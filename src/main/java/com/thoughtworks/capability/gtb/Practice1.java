c8957daa4cce960a4841540bfde50cfc6342f80e
chenzdido <chenzdido@gmail.com>
2020-09-17 19:22:25 +0800

    [zhe] feat:complete practice1.

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,11 @@
 package com.thoughtworks.capability.gtb;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.Period;
+
+import static java.time.temporal.ChronoUnit.DAYS;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -11,6 +16,10 @@ import java.time.LocalDate;
 public class Practice1 {
 
   public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
+    LocalDate mayDay = LocalDate.of(date.getYear(), 5, 1);
+    if(date.getMonth().getValue()>=5){
+      mayDay = mayDay.plusYears(1);
+    }
+    return DAYS.between(date,mayDay);
   }
 }
